0cea8e70da2ceafaa821c4bef7e723b38f62a689
Tsuyoshi Tokuda <108762+tokuda109@users.noreply.github.com>
2024-11-15 16:41:55 +0900

    fix the "'module' is not defined" error (#51)
    Release-As: 0.3.1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,13 @@
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
   {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
     ignores: ['node_modules'],
   },
   js.configs.recommended,
